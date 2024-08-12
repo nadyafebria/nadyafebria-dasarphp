e567b587e47cb778d2e1214b57bd3c566303a979
nadyafebria <nadyaksp694gmail.com>
2024-08-12 14:10:50 +0700

    belajar hari 1

---
+++ haripertama/coba.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>hallo semuanya </h1><br>" ;
+print "<h3>saya nadya </h3><br>" ;
+printf ("hai gusy hari ini kita pulang soree <br>");
---
+++ haripertama/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<h1>hallo semuanya </h1><br>" ;
+print "<h3>saya nadya </h3><br>" ;
+printf ("hari ini lagi pusing <br>");
+echo "_";
+?>
+<a href="coba.php">halaman index</a>
